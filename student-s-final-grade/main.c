a7f5db52c9e64800d8198bdd79e961f92c9eea6f
kekecatf <kekecatf@outlook.com>
2021-12-26 16:24:36 +0300

    create new kata

---
+++ student-s-final-grade/main.c
@@ -0,0 +1,7 @@
+// https://www.codewars.com/kata/5ad0d8356165e63c140014d4
+int final_grade(int exam, int projects){ 
+  return
+}
+
+printf(final_grade(100, 12), Equals(100));
+printf(final_grade(85, 5), Equals(90));
